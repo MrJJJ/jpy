bb9c82e2c1e2860a62c1ef5daa87dc104dfba280
mrjjj <jonathan.romiguier@gmail.com>
2015-07-02 15:28:39 +0200

    add phy2dic and changed fasta2dic

--- j.py
+++ j.py
@@ -1,6 +1,5 @@
 # encoding utf-8
 
-
 import math
 
 ########################
@@ -38,7 +37,7 @@ def test_multisort():
 	a,b,c = multisort([a,b,c])
 	print(a,b,c)
 	
-class csv(list): #Extension of class list to make cool stuff with csv
+class Csv(list): #Extension of class list to make cool stuff with csv
 	def __init__(self, *args):
 		list.__init__(self, *args)
 		self.headers = self[0] #headers attribute
@@ -52,6 +51,13 @@ class csv(list): #Extension of class list to make cool stuff with csv
 ###############	
 ###   seq   ###
 ###############
+def countACGT(seq):
+    return sum([float(seq.count(x)) for x in 'ACGT'])
+
+def pos(seq,posNumber):
+	s=seq[posNumber-1::3]
+	return(s)
+
 def gc(s): #GC% d'un string
 	return sum([float(s.count(x)) for x in 'GC']) / sum([float(s.count(x)) for x in 'ACGT'])
 
@@ -76,7 +82,7 @@ def gc3(s):
 
 
 def hamming(seq1,seq2): #Compte le nombre de diff entre 2 string (distance de Hamming)
-	return sum( [a != b for a,b in zip(seq1,seq2)] )
+	return sum( [a != b and a != 'N' and b != 'N' and a != '-' and b != '-' for a,b in zip(seq1,seq2)] )
 
 
 
@@ -90,11 +96,20 @@ def fasta2str(f): #Un alignement fasta en un string python
 
 def fasta2tuple(f):
 	fasta=open(f).readlines()
-	sp = [x.rstrip().replace('>','') for x in fasta if x[0] == '>']
+	seqName = [x.rstrip().replace('>','') for x in fasta if x[0] == '>']
 	seq = ''.join([x.rstrip() if x[0]!='>' else '@' for x in fasta])[1:].split('@')
-	return (sp,seq)
+	return (seqName,seq)
 
 def fasta2dic(f):
+	fastaStream = open(f)
+	fasta = fastaStream.readlines()
+	fastaStream.close()
+	sp = [x.replace('\n','').rstrip().replace('>','') for x in fasta if x[0] == '>']
+	seq = ''.join([x.rstrip() if x[0]!='>' else '@' for x in fasta])[1:].split('@')
+	dic = {k:v for k in sp for v in seq}
+	return dic 
+
+def fasta2richdic(f):
 	fastaStream = open(f)
 	fasta = fastaStream.readlines()
 	fastaStream.close()
@@ -105,6 +120,19 @@ def fasta2dic(f):
 	ngap = [ x.count('N')+x.count('-') for x in seq ]
 	return {'sp':sp,'seq':seq, 'gc3':GC3, 'len':length, 'ngap':ngap}
 
+def phy2dic(f):
+    phyStream = open(f)
+    phy = phyStream.readlines()
+    phyStream.close()
+    phyDic={}
+    for l in phy:
+        seqName = l.split()[0]
+        seq = l.split()[1]
+        if seqName in phyDic:
+            phyDic[seqName] = phyDic[seqName]+seq
+        else:
+            phyDic[seqName] = seq
+    return phyDic
 
 
 
@@ -124,33 +152,49 @@ def allFastaHaveSameNumberOfSpecies(globstr): #Test if all alignments have same 
 		b=[x for x in open(i).readlines() if x[0]=='>']
 		print(len(b))
 
+def gapHomogenization(seqList): #Take a list of seq and put N when not ACGT in all positions
+    seqListHomo = [['N']*len(seqList[0]) for i in range(len(seqList))]
+    for i in range(len(seqList[0])):
+        posIsToEliminate = 0
+        for j in range(len(seqList)):
+            if seqList[j][i] in 'ACGT':
+                pass
+            else:
+                posIsToEliminate = 1
+                break
+        if posIsToEliminate == 1:
+            pass
+        else:
+            for s in range(len(seqList)):
+                seqListHomo[s][i] = seqList[s][i]
+    return [''.join(x) for x in seqListHomo]
 
 ################
 ###   tree   ###
 ################
 
 
-def treeToTaxa(tree): # treeTotaxa : renvoie une liste des noms des noeuds du gene mais ou les noeuds internes sont designes par l'ensemble des espèces qu'ils contiennent (sp1_sp2_..._spN)
+def treeToTaxa(tree): # treeTotaxa : renvoie une liste des noms des noeuds du gene mais ou les noeuds internes sont designes par l'ensemble des especes qu'ils contiennent (sp1_sp2_..._spN)
 	nbNode = tree.count(":") #compte le nombre de ':' (=noeuds)
 #	print("Il y a",nbNode,"noeuds")
 	taxas=[] #Tableau qui va acceuillir tous les noms de taxons
 	indexNode = [] # Tableau qui va acceuillir toutes les positions des ':' (=noeuds)
 	for i in range(0,nbNode):
-		indexNode.append(tree.replace(":","X",i).find(":")) #trouve la position du ième ':' et la stocke dans le tableau indexNode
+		indexNode.append(tree.replace(":","X",i).find(":")) #trouve la position du ieme ':' et la stocke dans le tableau indexNode
 	for i in indexNode:
 		j=1
 		char=tree[i-j]
-		while char.isdigit() or char==' ' or char=='.': #Remonte depuis le ':' jusqu'à la première lettre ou première parenthèse qu'il rencontre
+		while char.isdigit() or char==' ' or char=='.': #Remonte depuis le ':' jusqu'a la premiere lettre ou premiere parenthese qu'il rencontre
 			j+=1
 			char=tree[i-j]
 		k=j # sauvegarde de la position j
-		if char.isalpha():  # Si c'est une lettre (ou underscore pour 2 espèces de tortues)
+		if char.isalpha():  # Si c'est une lettre (ou underscore pour 2 especes de tortues)
 			while char.isalpha() or char=='_': #Tant que c'est une lettre on remontre pour chopper tout le nom de l'espece
 				k+=1
 				char=tree[i-k]
-			taxa=tree[i-k+1:i-j+1] # =le nom de l'espèce
+			taxa=tree[i-k+1:i-j+1] # =le nom de l'espece
 			taxas.append(taxa) #Ajout du nom de l'sp dans le tableau taxas
-		if char==')':       # Si c'est une parenthèse
+		if char==')':       # Si c'est une parenthese
 			parenthesisSum=-1
 			while parenthesisSum!=0: # on cherche la parenthese ouvrante correspondante pour avoir toutes les especes qui sont contenues dans la branche ancestrale correspondante. 
 				k+=1
@@ -181,25 +225,25 @@ def treeToTaxa(tree): # treeTotaxa : renvoie une liste des noms des noeuds du ge
 
 
 
-def taxaConversion(taxas,spphylo): # Prend tableau taxas crée avec fonction treeToTaxa et fichier spphylo (=1 ligne par espèces avec toute leur phylogénie, ex : Mus,Rodentia,...,Mammal) -> renvoie une liste des noms des noeuds du gène mais où les noeuds internes sont désignés par l'ensemble des espèces qu'ils contiennent (sp1_sp2_..._spN)
-	taxasConverted=[] # Résultat final de la fonction = Liste qui va contenir toutes les conversion
+def taxaConversion(taxas,spphylo): # Prend tableau taxas cree avec fonction treeToTaxa et fichier spphylo (=1 ligne par especes avec toute leur phylogenie, ex : Mus,Rodentia,...,Mammal) -> renvoie une liste des noms des noeuds du gene mais ou les noeuds internes sont designes par l'ensemble des especes qu'ils contiennent (sp1_sp2_..._spN)
+	taxasConverted=[] # Resultat final de la fonction = Liste qui va contenir toutes les conversion
 	for taxa in taxas: #Pour chaque noeuds non convertits
 		taxa=taxa.split(',')#On saucissone tous les '_'
 		if len(taxa) > 1: #Si on est dans un noeud interne
-			phylodico = {} #Dico qui va associer chaque taxon au nombre de fois où rencontrera dans spphylo
-			for sp in taxa: #Pour chaque espèce dans taxa -> On va créer un dico
+			phylodico = {} #Dico qui va associer chaque taxon au nombre de fois ou rencontrera dans spphylo
+			for sp in taxa: #Pour chaque espece dans taxa -> On va creer un dico
 				for pline in spphylo: #On parourt chaque ligne de spphylo
 					pline=pline.split(',')#On saucissone selon les ','
-					if pline[0] == sp: #On cherche ligne de l'espèce orrespondante dans spphylo
-						phyloorder=pline #Sauvegarde d'une ligne de spphylo avec bonne espèce
+					if pline[0] == sp: #On cherche ligne de l'espece orrespondante dans spphylo
+						phyloorder=pline #Sauvegarde d'une ligne de spphylo avec bonne espece
 						for p in pline: #Pour chaque case de la ligne de spphylo
 							if p in phylodico: 
-								phylodico[p]+=1 #Si dans dico, on ajoute 1 au nombre de fois où on l'a rencontrée
+								phylodico[p]+=1 #Si dans dico, on ajoute 1 au nombre de fois ou on l'a rencontree
 							else:
-								phylodico[p]=1 #Si pas dans le dico on l'initialise à 1.
-			position=9999999999999999 #On initialise position en dernière position
-			for cle in phylodico: #Pour chaque clé du dico
-				if phylodico[cle]==max(phylodico.values()): #On sélectionne toutes les clés qui ont valeur maximale
+								phylodico[p]=1 #Si pas dans le dico on l'initialise a 1.
+			position=9999999999999999 #On initialise position en derniere position
+			for cle in phylodico: #Pour chaque cle du dico
+				if phylodico[cle]==max(phylodico.values()): #On selectionne toutes les cles qui ont valeur maximale
 					if phyloorder.index(cle) < position:
 						position=phyloorder.index(cle)
 			taxaConverted=phyloorder[position]
@@ -216,14 +260,14 @@ def treeToGC(tree): # arbre -> liste des labels (bootstraps, GC de nhml)
 	GC=[]                    # Tableau qui va acceuillir toutes les valeurs de GC
 	indexNode = []				# Tableau qui va acceuillir toutes les positions des ':' (=noeuds)
 	for i in range(0,nbNode):
-		indexNode.append(tree.replace(":","X",i).find(":"))  # Trouve la position du ième ':' et la stocke dans le tableau indexNode
+		indexNode.append(tree.replace(":","X",i).find(":"))  # Trouve la position du ieme ':' et la stocke dans le tableau indexNode
 	for i in indexNode:
 		j=1
 		char=tree[i-j]
 		while char.isdigit() or char=='.':
 			j+=1
 			char=tree[i-j]
-		GCsp=tree[i-j+1:i]       # On garde la partie correspondante à la valeur de GC
+		GCsp=tree[i-j+1:i]       # On garde la partie correspondante a la valeur de GC
 		GC.append(GCsp)
 		#print(char)
 	return GC
@@ -236,14 +280,14 @@ def treeToBL(tree): # arbre -> liste des longueurs de branches
 	BL=[]                    # Tableau qui va acceuillir toutes les valeurs de longueur de branches (BL)
 	indexNode = []				# Tableau qui va acceuillir toutes les positions des ':' (=noeuds)
 	for i in range(0,nbNode):
-		indexNode.append(tree.replace(":","X",i).find(":"))  # Trouve la position du ième ':' et la stocke dans le tableau indexNode
+		indexNode.append(tree.replace(":","X",i).find(":"))  # Trouve la position du ieme ':' et la stocke dans le tableau indexNode
 	for i in indexNode:
 		j=1
 		char=tree[i+j]
 		while char.isdigit() or char=='.':
 			j+=1
 			char=tree[i+j]
-		BLsp=tree[i+1:i+j]       # On garde la partie correspondante à la valeur de longueur de branhe
+		BLsp=tree[i+1:i+j]       # On garde la partie correspondante a la valeur de longueur de branhe
 		BL.append(BLsp)
 		#print(char)
 	return BL
